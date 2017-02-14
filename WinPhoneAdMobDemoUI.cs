91396d6f3a3a94caf04b145a91ce86529553b5cd
batmannolove <2647426257@qq.com>
2017-02-14 10:47:44 -0500

    initial commit

---
+++ WinPhoneAdMobDemoUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+using Prime31.WinPhoneAdMob;
+
+
+
+public class WinPhoneAdMobDemoUI : MonoBehaviourGUI
+{
+#if UNITY_WP8
+	
+	void Start()
+	{
+				WinPhoneAdMob.createBanner( "ca-app-pub-6962962754220734/5837644807", AdFormat.Banner, AdHorizontalAlignment.Center, AdVerticalAlignment.Top, true );
+
+	}
+
+#endif
+}
